cd95234276e1e11520a5edb52dcf61a1a8e44f49
HyoungDong <52812307+HyoungDong@users.noreply.github.com>
2022-02-08 22:44:02 +0900

    add dark-mode function

--- virtual-keyboard/src/js/index.js
+++ virtual-keyboard/src/js/index.js
@@ -1,3 +1,14 @@
 import '../css/style.css';
 
-console.log('hi');
+function toggleThemeMode() {
+  const root = document.getElementsByTagName('html')[0];
+  console.log(root.Theme);
+  if (root.getAttribute('theme') === 'dark-mode') root.setAttribute('theme', '');
+  else root.setAttribute('theme', 'dark-mode');
+}
+
+const toggleSwitch = document.getElementsByClassName('switch')[0];
+
+toggleSwitch.addEventListener('click', e => {
+  if (e.target.id === 'switch') toggleThemeMode();
+});
