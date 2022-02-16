b35e4dfedfd8e4b32233184af847650b46cde646
HyoungDong <52812307+HyoungDong@users.noreply.github.com>
2022-02-16 19:47:19 +0900

    add function that image slided by indicator clicked event

--- image-slide/src/js/imageSlider.js
+++ image-slide/src/js/imageSlider.js
@@ -4,23 +4,62 @@ export default function imageSlider() {
   this.sliderListEl = this.sliderWrapEl.querySelector('#slider');
   this.nextBtnEl = this.sliderWrapEl.querySelector('#next');
   this.prevBtnEl = this.sliderWrapEl.querySelector('#previous');
+  this.indicatorWrapEl = this.sliderWrapEl.querySelector('#indicator-wrap');
   this.slideNumber = this.sliderListEl.querySelectorAll('li').length;
   this.slideWidth = this.sliderListEl.clientWidth;
   this.sliderListEl.style.width = `${this.slideNumber * this.slideWidth}px`;
-  this.addEvent.call(this);
+  this.addEvent();
+  this.createIndicator();
+  this.setIndicator();
 }
 
 imageSlider.prototype.addEvent = function () {
-  this.nextBtnEl.addEventListener('click', e => {
-    this.currentPos++;
-    this.currentPos %= this.slideNumber;
-    console.log(this.currentPos);
-    this.sliderListEl.style.left = `-${this.slideWidth * this.currentPos}px`;
-  });
+  this.nextBtnEl.addEventListener('click', this.nextBtnEvent.bind(this));
 
-  this.prevBtnEl.addEventListener('click', e => {
-    this.currentPos--;
-    this.currentPos = (this.currentPos + this.slideNumber) % this.slideNumber;
+  this.prevBtnEl.addEventListener('click', this.prevBtnEvent.bind(this));
+
+  this.indicatorWrapEl.addEventListener(
+    'click',
+    this.indicatorEvent.bind(this),
+  );
+};
+
+imageSlider.prototype.indicatorEvent = function (e) {
+  const indexPos = parseInt(e.target.dataset.index);
+  if (Number.isInteger(indexPos)) {
+    this.currentPos = indexPos;
     this.sliderListEl.style.left = `-${this.slideWidth * this.currentPos}px`;
-  });
+    this.setIndicator();
+  }
+};
+
+imageSlider.prototype.nextBtnEvent = function (e) {
+  this.currentPos++;
+  this.currentPos %= this.slideNumber;
+  this.sliderListEl.style.left = `-${this.slideWidth * this.currentPos}px`;
+  this.setIndicator();
+};
+
+imageSlider.prototype.prevBtnEvent = function (e) {
+  this.currentPos--;
+  this.currentPos = (this.currentPos + this.slideNumber) % this.slideNumber;
+  this.sliderListEl.style.left = `-${this.slideWidth * this.currentPos}px`;
+  this.setIndicator();
+};
+
+imageSlider.prototype.createIndicator = function () {
+  const docFragment = document.createDocumentFragment();
+  for (let i = 0; i < this.slideNumber; i++) {
+    const li = document.createElement('li');
+    li.dataset.index = i;
+    docFragment.appendChild(li);
+  }
+  this.indicatorWrapEl.querySelector('ul').appendChild(docFragment);
+};
+
+imageSlider.prototype.setIndicator = function () {
+  this.indicatorWrapEl.querySelector('li.active')?.classList.remove('active');
+  this.indicatorWrapEl
+    .querySelector(`ul li:nth-child(${this.currentPos + 1})`)
+    .classList.add('active');
 };
